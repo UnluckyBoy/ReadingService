b75b0758702b4947ee2a5036467d247c41e2373b
UnluckyBoy <qfmy_zhang@163.com>
2024-08-31 16:35:50 +0800

    添加其他功能 By Matrix

---
+++ src/main/java/com/cloudstudio/readingservice/controller/WebController.java
@@ -0,0 +1,17 @@
+package com.cloudstudio.readingservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @ClassName WebController
+ * @Author Create By matrix
+ * @Date 2024/8/31 15:44
+ */
+@Controller
+public class WebController {
+    @RequestMapping("/index")
+    public String Index(){
+        return "index";
+    }
+}
